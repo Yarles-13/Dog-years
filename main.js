81a35254309209caec3b8f2e33fa0c1769e7598c
Yarles-13 <127643126+Yarles-13@users.noreply.github.com>
2024-04-23 10:37:31 -0500

    First/final draft of Dog years

---
+++ main.js
@@ -0,0 +1,14 @@
+const myAge = 24;
+let myName= "Yardley".toLowerCase();
+
+
+
+let earlyYears = 2;
+earlyYears *= 10.5;
+
+let laterYears = myAge - 2;
+laterYears *= 4;
+
+let myAgeinDogYears = earlyYears + laterYears;
+
+console.log( `My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeinDogYears} years old in dog years `)
